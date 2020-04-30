c1423b68f3b568285a23dd29b8510e6917f0dbad
Siva Prasad <siva.sesi.59@gmail.com>
2020-04-30 16:57:42 -0700

    third excerise from basics chapter in LS ruby book

---
+++ the_basics/hash_movie_year.rb
@@ -0,0 +1,7 @@
+movie_titles = {:Irishmen => 2019,
+                :Avatar => 2009,
+                :Moonlight => 2018}
+
+puts movie_titles[:Irishmen]
+puts movie_titles[:Avatar]
+puts movie_titles[:Moonlight]
