7ac1032e8017bee127fc80bfa3ff20ae4d519185
Siva Prasad <siva.sesi.59@gmail.com>
2020-04-30 18:09:02 -0700

    second exercise from chapter2 Variables in LS ruby book

---
+++ Variables_chapter2/age.rb
@@ -0,0 +1,11 @@
+puts "How old are you??"
+answer = gets.chomp.to_i
+age_10 = answer + 10
+age_20 = answer + 20
+age_30 = answer + 30
+age_40 = answer + 40
+
+puts "You will be #{age_10} in 10 years"
+puts "You will be #{age_20} in 20 years"
+puts "You will be #{age_30} in 30 years"
+puts "You will be #{age_40} in 40 years"
