4b8bd32a83f865542bc0e8b12247f1fa7a4eaf2a
Siva Prasad <siva.sesi.59@gmail.com>
2020-04-30 17:03:48 -0700

    Fourth exercise from basics chapter in LS ruby book

---
+++ the_basics/array_movie_dates.rb
@@ -0,0 +1,8 @@
+movie_titles = {:Irishmen => 2019,
+                :Avatar => 2009,
+                :Moonlight => 2018}
+
+array_movies = [movie_titles[:Irishmen], movie_titles[:Avatar], movie_titles[:Moonlight]]
+puts array_movies[0]
+puts array_movies[1]
+puts array_movies[2]
