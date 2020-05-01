d99554b7b4af1d3d21c93981ed261ce5c7d7f623
Siva Prasad <siva.sesi.59@gmail.com>
2020-05-01 16:15:15 -0700

    fifth exercise from Chapter7 hashes in LS ruby book

---
+++ hashes_chapter7/specific_value_hash.rb
@@ -0,0 +1,11 @@
+details = {name: "Siva",
+           age:  "30",
+           height: "6ft",
+           hobby: "coding"}
+
+
+if details.has_value?("Siva")
+  puts "Value is present"
+else
+  puts "Value is not present in hash"
+end
