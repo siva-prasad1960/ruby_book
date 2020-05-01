6728a2b2aed360d886bc08035e94a133e8b90db3
Siva Prasad <siva.sesi.59@gmail.com>
2020-04-30 17:12:03 -0700

    fifth excerise from basics chapter in LS ruby book

---
+++ the_basics/factorial.rb
@@ -0,0 +1,8 @@
+factorial_5 = 5 * 4 * 3 * 2 * 1
+puts "factorial of number 5 is #{factorial_5}"
+factorial_6 = 6 * 5 * 4 * 3 * 2 * 1
+puts "factorial of number 6  is #{factorial_6}"
+factorial_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts "factorial of number 7 is #{factorial_7}"
+factorial_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts "factorial of number 8 is #{factorial_8}"
